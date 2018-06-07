c801eb07b42ea1826427fb4329636aa80784b0a3
lvjibin <lvjibin@fotoable.com>
2018-06-07 10:32:47 +0800

    creat

---
+++ FrameWork/FrameWorkCore/FrameWorkCore/Core/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameWorkCore.Core
+{
+    class Test
+    {
+        public void Log(string logstring)
+        {
+            Console.WriteLine(logstring);
+        }
+    }
+}
