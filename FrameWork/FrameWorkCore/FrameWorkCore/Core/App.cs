9115ca29acff84a41023f98c7b336f9af6e5855d
lvjibin <lvjibin@fotoable.com>
2018-11-15 11:47:41 +0800

    add

---
+++ FrameWork/FrameWorkCore/FrameWorkCore/Core/App.cs
@@ -0,0 +1,10 @@
+namespace FrameWorkCore.Core
+{
+    public class App
+    {
+        public static void Init()
+        {
+            
+        }
+    }
+}
--- FrameWork/FrameWorkCore/FrameWorkCore/Core/Test.cs
+++ FrameWork/FrameWorkCore/FrameWorkCore/Core/Test.cs
@@ -10,7 +10,22 @@ namespace FrameWorkCore.Core
     {
         public void Log(string logstring)
         {
-            Console.WriteLine(logstring);
+            RandomValue randomvalue = new RandomValue();
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(randomvalue.Nest());
+            }
+
+            randomvalue.SetRange(1,100);
+            Console.WriteLine("==============");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(randomvalue.Nest());
+            }
+
+            //Console.WriteLine(new NumberFactory().GetCentCout(19.97923f));
+            //Console.WriteLine(logstring);
         }
     }
 }
