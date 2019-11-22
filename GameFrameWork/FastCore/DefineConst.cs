c3bcecad390f04a2b2ab62649147ae245569c2ce
lv <517752548@qq.com>
2019-11-22 08:45:19 +0800

    add

---
+++ GameFrameWork/FastCore/DefineConst.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefineConst
+{
+    public const string Addressable = "Define_Addressable";
+    public const string Notification = "Define_Notification";
+}
