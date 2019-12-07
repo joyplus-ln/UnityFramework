514a5765fe6701fdfd11f12cfad221d9717ee41e
lv <517752548@qq.com>
2019-12-08 07:56:49 +0800

    add

---
+++ GameFrameWork/FastCore/Editor/Bundle/BundleMenuTool.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BundleMenuTool
+{
+    [MenuItem ("Fast/Bundle/ReInit")]
+    static void CopyAssetPath ()
+    {
+       
+    }  
+}
